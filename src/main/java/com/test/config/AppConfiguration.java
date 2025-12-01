60348cad492e6f360e01c5db26c494572d04d769
j-z-frydrychowski <s32713@pjwstk.edu.pl>
2025-12-01 02:22:47 +0100

    Dodanie klasy AppConfiguration, która będzie używana jako Dummy

---
+++ src/main/java/com/test/config/AppConfiguration.java
@@ -0,0 +1,6 @@
+package com.test.config;
+
+// Klasa konfiguracyjna, która będzie używana jako Dummy
+public class AppConfiguration {
+    public String getDbUrl() { return "jdbc:mysql://localhost:3306/db"; }
+}
